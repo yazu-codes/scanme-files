7f1746cceabc98e348ee2dd3670719edac02ae9d
GOGO <georgi.yazovaliiski@gmail.com>
2026-07-16 14:25:38 +0300

    paths

--- src/util/config.go
+++ src/util/config.go
@@ -23,7 +23,7 @@ func (c *ConfigReader) Setup() {
 	// config = "a"
 	if config != "" {
 		fmt.Println("CONFIG_YAML environment variable is set. Writing to config.yaml.")
-		err := os.WriteFile(".\\configs\\config.yaml", []byte(config), 0600)
+		err := os.WriteFile("..\\..\\configs\\config.yaml", []byte(config), 0600)
 		if err != nil {
 			fmt.Println("Error writing file.")
 			panic(err)
@@ -34,7 +34,7 @@ func (c *ConfigReader) Setup() {
 
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath("..\\..\\configs") // current directory
+	viper.AddConfigPath("configs") // current directory
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("failed to read config: %v", err)
